a2266dc5263a00b2b4b456ac80fde57f13a6bba0
Joe Rezendes <rezenbiz@gmail.com>
2017-12-23 01:00:33 -0800

    Finished Product

---
+++ public/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function()
+{
+
+
+
+
+
+
+  $(".portmation").hover(
+      function () {
+        $(this).addClass('animated pulse infinite');
+      },
+      function () {
+        $(this).removeClass("animated pulse infinite");
+      }
+    );
+
+
+
+});
